afb4ceba933cc83738d198030b66e98f9893c09e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-08 19:17:18 +0000

    use puts function to print string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - This function prints a string using puts 
+ * main - This function prints a string using puts
  * Return: 0
  */
 
 
 int main(void)
 {
-	char *str = "Programming is like building a multilingual puzzle.";
+	char *str = "\"Programming is like building a multilingual puzzle.\"";
 
 	puts(str);
 	return (0);
