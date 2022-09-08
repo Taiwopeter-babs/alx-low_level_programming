09e68bd76c3d8fb7a15127125411d8f343e27db5
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-08 19:42:39 +0000

    print using printf and checks with gcc Wall

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - The function prints a string with printf
+ * Return: 0
+ */
+int main(void)
+{
+	char *str = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+}
