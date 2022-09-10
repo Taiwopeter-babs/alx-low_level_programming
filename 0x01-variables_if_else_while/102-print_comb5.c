67711d5cd07749406b84da996cd6f4006787dffa
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-10 10:44:43 +0000

    combine two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints unique combunation of two two-digit numbers
+ *
+ * Description:
+ * Return: 0
+ */
+int main(void)
+{
+	int dg1, dg2;
+
+	for (dg1 = 0; dg1 <= 98; dg1++)
+	{
+		for (dg2 = dg1 + 1; dg2 <= 99; dg2++)
+		{
+			putchar(dg1 / 10 + '0');
+			putchar(dg1 % 10 + '0');
+
+			putchar(' ');
+
+			putchar(dg2 / 10 + '0');
+			putchar(dg2 % 10 + '0');
+
+
+			if (dg1 + dg2 == 197)
+				break;
+
+			putchar(' ');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
