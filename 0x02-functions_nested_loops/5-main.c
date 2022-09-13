44d472aa25a3a2bf317e98c2468e452f94e67096
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-13 14:29:55 +0000

    differentiate between positive and negative values

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - calls the func
+ *
+ * Return: 0
+ */
+int main(void)
+{	
+	print_sign(789776);
+	/*_putchar(t + '0');*/
+	print_sign(-87);
+	/*_putchar(t + '0');*/
+	print_sign(0);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the case of the parameter
+ * @n: paramter parsed to function
+ *
+ * Description: a single value is parsed as a parameter
+ * and the function checks if the paramter is positive, negative
+ * or is zero (0). Prints "+" for positive parameters, prints "-'
+ * for negative parameters, and prints "0" for 0 value.
+ * Return: 1 if value is positive, otherwise 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('1');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('-');
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0');
+	}
+
+	_putchar('\n');
+	return (0);
+}
