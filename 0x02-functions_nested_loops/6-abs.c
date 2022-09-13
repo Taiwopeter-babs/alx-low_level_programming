b70b4cbb35134a627e12fd470c36472d571d36cd
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-13 15:00:27 +0000

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * @n: paramter parsed to function
+ *
+ * Description: a single value is parsed as a parameter
+ * and the function checks if the paramter is positive, negative
+ * or is zero (0). Returns the non-negative values
+ * Return: 0
+ */
+int _abs(int n)
+{
+	int val = -1;
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (n * val);
+	}
+
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - calls the func
+ *
+ * Return: 0
+ */
+int main(void)
+{	
+	_abs(-98);
+	_putchar('\n');
+	return (0);
+}
