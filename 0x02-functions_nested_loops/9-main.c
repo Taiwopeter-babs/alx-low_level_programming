9d3ca94b0c71a4c4becd37e5a30388dc343c4a9e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-13 20:21:36 +0000

    display the multiplication table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - calls the func
+ *
+ * Return: 0
+ */
+int main(void)
+{	
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints multiplication table to 9
+ *
+ * Return: 0 (success)
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (i * j >= 10)
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i * j < 10)
+			{
+				_putchar(i * j + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+			_putchar('\n');
+	}
+
+}
