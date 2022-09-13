ee92b242facabcfee3ea0ab99c04b78dcded840c
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-13 17:41:17 +0000

    display 24 hours of jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of 24 hours
+ *
+ * Return: 0 (success)
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			/*_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');*/
+
+			while (j < 10)
+			{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			j++;
+			_putchar('\n');
+			}
+			
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+
+		}
+	}
+
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - calls the func
+ *
+ * Return: 0
+ */
+int main(void)
+{	
+	jack_bauer();
+	return (0);
+}
