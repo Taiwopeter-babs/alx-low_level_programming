b8ddd522f83f5da51a0e9408210c30fa3aa281dd
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-14 12:52:33 +0000

    display multiplication table for integer n

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,32 +12,37 @@ void print_times_table(int n)
 
 	if (n > 0 && n <= 15)
 	{
-	for (i = 0; i <= n; ++i)
+		for (i = 0; i <= n; ++i)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; ++j)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_product = i * j;
+
+				if (_product <= 99)
+					_putchar(' ');
+				if (_product <= 9)
+					_putchar(' ');
+
+				if (_product >= 100)
+				{
+					_putchar((_product / 100) + '0');
+					_putchar((_product / 10) % 10 + '0');
+				}
+				else if (_product <= 99 && _product >= 10)
+					_putchar((_product / 10) + '0');
+				_putchar((_product % 10) + '0');
+
+			}
+			_putchar('\n');
+		}
+	}
+	else
 	{
 		_putchar('0');
-		for (j = 1; j <= n; ++j)
-		{
-			_putchar(',');
-			_putchar(' ');
-			_product = i * j;
-
-			if (_product <= 99)
-				_putchar(' ');
-			if (_product <= 9)
-				_putchar(' ');
-
-
-			if (_product >= 100)
-			{
-				_putchar((_product / 100) + '0');
-				_putchar((_product / 10) % 10 + '0');
-			}
-			else if (_product <= 99 && _product >= 10)
-				_putchar((_product / 10) + '0');
-			_putchar((_product % 10) + '0');
-
-		}
 		_putchar('\n');
 	}
-	}
+
 }
