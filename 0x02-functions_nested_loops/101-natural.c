05b0502da8e1dc96587e83a5dc4892aebd504c44
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-14 14:03:27 +0000

    test main function

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * multiple_of_three - compute the sum of multiples of 3 & 5
+ * main - compute the sum of multiples of 3 & 5
  * Return: 0 (success)
  */
-void multiple_of_three(void)
+int main(void)
 {
 	int i, t_sum = 0;
 
@@ -13,5 +13,7 @@ void multiple_of_three(void)
 			t_sum += i;
 	}
 	printf("%d\n", t_sum);
+	
+	return (0);
 
 }
