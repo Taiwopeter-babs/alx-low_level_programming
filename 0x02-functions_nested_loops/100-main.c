39fff3f237ed9fd0ce53b69dd412ee40c7233f87
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-14 12:38:21 +0000

    display multiplication table for integer n

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - calls the func
+ *
+ * Return: 0
+ */
+int main(void)
+{	
+	print_times_table(16);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints multiplication table to n
+ * @n: value the multiplication table is based on
+ * Return: 0 (success)
+ */
+void print_times_table(int n)
+{
+	int i, j;
+	int _product;
+
+	if (n > 0 && n <= 15)
+	{
+	for (i = 0; i <= n; ++i)
+	{
+		_putchar('0');
+		for (j = 1; j <= n; ++j)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_product = i * j;
+
+			if (_product <= 99)
+				_putchar(' ');
+			if (_product <= 9)
+				_putchar(' ');
+
+			if (_product > n && _product < 10)
+			{
+				_putchar(' ');
+				_putchar((_product % 10) + '0');
+			}
+
+			if (_product >= 100)
+			{
+				_putchar((_product / 100) + '0');
+				_putchar((_product / 10) % 10 + '0');
+			}
+			else if (_product <= 99 && _product >= 10)
+				_putchar((_product / 10) + '0');
+			_putchar((_product % 10) + '0');
+
+		}
+		_putchar('\n');
+	}
+	}
+}
