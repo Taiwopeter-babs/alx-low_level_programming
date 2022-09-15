d9d78b46e0ab3a33d4a7ee432a4d8ca62c12cad3
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-15 23:41:07 +0000

    display fizzbuzz and triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle of characters of size
+ * @size: size of the triagle
+ *
+ * Description: function takes n as a parameter and prints "#"
+ * in the shape of triangle.
+ * Example:
+ *	print_triangle(2); --> #
+ *			      ##
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (i + j <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+				
+			}
+			_putchar('\n');
+		}
+	}
+	else if (size <= 0)
+		_putchar('\n');
+}
