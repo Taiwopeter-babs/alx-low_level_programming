2703853a036a9d6e4fb3dfcdfb76f57e7dd0493f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-15 16:10:44 +0000

    print diagonal slash

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls the function in code block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	print_diagonal(10);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: parameter
+ *
+ * Description: function takes n as a parameter and prints "\"
+ * n times.
+ * Example:
+ *	print_line(2); --> \
+ *			    \
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+					/*_putchar(' ');*/
+				}
+				else if (i > j)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	else if (n <= 0)
+		_putchar('\n');
+}
