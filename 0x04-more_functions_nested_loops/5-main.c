60f9187b59cc64659191ad01cbba224e53e54a1f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-15 14:06:53 +0000

    testing numbers

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls the function in code block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	more_numbers();
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 on ten lines
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i = 0, j;
+	char arr[] = "01234567891011121314";
+
+	while (i < 10)
+	{
+		j = 0;
+
+		while (arr[j])
+		{
+			_putchar(arr[j]);
+			
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+
+}
