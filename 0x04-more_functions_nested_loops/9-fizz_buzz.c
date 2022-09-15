b62385342480cb1a3d23600275123e7cf571b143
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-15 19:18:10 +0000

    test for fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,7 @@
  * main - prints integers from 1- 100, then fizz-buzz
  *
  * Description: function prints numbers from 1-100, then substitutes
- * multiples of 3 for fizz, multiples of 5 for buzz, and multiples 
+ * multiples of 3 for fizz, multiples of 5 for buzz, and multiples
  * of both 3 and 5 for fizzbuzz
  * Example:
  *	main() --> 1, 2, fizz, 4, buzz,..., 14, fizzbuzz...
