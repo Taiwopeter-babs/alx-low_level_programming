7b652965302d3910501e59c41ec6e25155dbeece
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-16 13:36:43 +0000

    fix leap year calculator

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	int month, day, year;
 
-	month = 9;
-	day = 16;
-	year = 2022;
+	month = 2;
+	day = 29;
+	year = 2020;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
