de88d32b2d5fb3bc005cae4f084893271495a4d2
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-19 20:38:27 +0000

    print elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	i = 0;
+
+	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+		i++;
 	}
-	printf("%d", a[i]);
 	printf("\n");
 
 }
