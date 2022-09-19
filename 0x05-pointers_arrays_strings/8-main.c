ef81171e4d142e7b26aeeafe1511785b67111681
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-19 19:57:28 +0000

    prints elements of array

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls the function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int s[] = {1, 4, 5, 7, 11, 13, 17};
+
+
+	print_array(s, 5);
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints the amount of n spaecified for the array
+ * @a: array to be printed from
+ * @n: number of elements to print
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 2; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("%d", *(a + i));
+	printf("\n");
+
+}
+
