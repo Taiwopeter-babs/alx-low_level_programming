4372e6b85c078b7552f8b5027d11f21f485db1f4
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-19 17:46:44 +0000

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,16 +1,16 @@
 #include "main.h"
-int string_length(char*);
+int string_length(char *string);
 
 /**
  * rev_string - prints a string in reverse
  * @s: paramter to be printed
  *
- * Description: The key part of this function is in the swapping of the 
+ * Description: The key part of this function is in the swapping of the
  * characters. Iterate through for index less than half of the value
  * of the length. This is because as @dev swaps each starting and ending
  * value, the string is divided into half.
  *
- * Example: say *s = "Taiwo babs" -> sbab owiaT  
+ * Example: say *s = "Taiwo babs" -> sbab owiaT
  * Return: void
  */
 void rev_string(char *s)
@@ -36,18 +36,19 @@ void rev_string(char *s)
 		ch = *last_char;
 		*last_char = *first_char;
 		*first_char = ch;
-		
+
 		first_char++;
 		last_char--;
-		
+
+
 	}
 }
 
 /**
  * string_length - computes the length
- * @stri: parameter string
+ * @string: parameter string
  *
- * Return: length 
+ * Return: length
  */
 int string_length(char *string)
 {
