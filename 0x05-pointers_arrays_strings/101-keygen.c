0169ecd25f6bc853323ba7421cf7288dc3c711b4
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-20 14:45:33 +0000

    generate random numbers for passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - generate random valid passwords
+ *
+ * Description: Initialize the int sign and unsigned num to 1 and 0
+ * This is to ensure tracking is done with each additon of digit
+ * The unsigned int num and i is used to contain large numerical
+ * 
+ * Return: integer converted
+ */
+int main(void)
+{
+	int seed, *_password;
+
+	seed = time(0);
+
+	srand(seed);
+	
+	_password = (int *) malloc(10 * sizeof(int));
+
+	*_password = rand();
+
+	printf("%d\n", *_password);
+
+	return (0);	
+}
+
