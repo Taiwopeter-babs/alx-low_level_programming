4477ae229abe759f65edab81fff6a4357cf7fb13
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-20 15:06:17 +0000

    generate random numbers for passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,19 +6,13 @@
 /**
  * main - generate random valid passwords
  *
- * Description: Initialize the int sign and unsigned num to 1 and 0
- * This is to ensure tracking is done with each additon of digit
- * The unsigned int num and i is used to contain large numerical
- * 
  * Return: integer converted
  */
 int main(void)
 {
-	int seed, _password;
+	int _password;
 
-	seed = time(0);
-
-	srand(seed);
+	srand(time(0));
 	
 	/* _password = (int *) malloc(10 * sizeof(int)); */
 
