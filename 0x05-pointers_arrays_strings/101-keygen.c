633bd5f30847e83632a85d3b665f9e40608eb3d2
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-20 20:31:41 +0000

    update to random passowrd generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * main - generate random valid passwords
@@ -10,15 +11,49 @@
  */
 int main(void)
 {
-	int *_password;
 
-	srand(time(NULL));
+	int index = 0, make_random = 0, len_of_password = 10;
 
-	_password = (int *) malloc(10 * sizeof(int));
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	char LETTERS[] = "ABCDEFGHIJLKMNOPQRSTUVWXYZ";
+	char digits[] = "0123456789";
+	char symbols[] = ",.!@#$";
 
-	*_password = rand();
+	srand((unsigned int)(time(NULL)));
 
-	printf("%i\n", *_password);
+	char _password[len_of_password];
+
+	make_random = rand() % 4;
+
+	for (index = 0; index < len_of_password; index++)
+	{
+		if (make_random == 1)
+		{
+			_password[index] = digits[rand() % 10];
+			make_random = rand() % 4;
+			printf("%c", _password[index]);
+		}
+		else if (make_random == 2)
+		{
+			_password[index] = symbols[rand() % 6];
+			make_random = rand() % 4;
+			printf("%c", _password[index]);
+		}
+		else if (make_random == 3)
+		{
+			_password[index] = LETTERS[rand() % 26];
+			make_random = rand() % 4;
+			printf("%c", _password[index]);
+		}
+		else if (make_random == 4)
+		{
+			_password[index] = letters[rand() % 26];
+			make_random = rand() % 4;
+			printf("%c", _password[index]);
+		}
+	}
+	printf("\n");
+
 
 	return (0);
 }
