3cb7ebc7e4f75033f57a4c8ae78e00bdb796bfe4
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-21 22:33:09 +0000

    update code to suit ascii value comparison

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,31 +15,18 @@
 int _strcmp(char *s1, char *s2)
 {
 	 int flag = 0;
+	 int i = 0;
 
-	while (*s1 != *s2 && *s1 != '\0')
+	while (flag == 0)
 	{
-		if (*s1 == *s2)
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 		{
-			flag += 0;
+			break;
 		}
-		else if (*s1 < *s2)
-		{
-			flag += -5;
-		}
-		else if (*s1 == '\0' && *s2 != '\0')
-		{
-			flag += -5;
-		}
-		else if (*s1 != '\0' && *s2 == '\0')
-		{
-			flag += 5;
-		}
-		else if (*s1 > *s2)
-		{
-			flag += 5;
-		}
+		flag += *(s1 + i) - *(s2 + i);
 		s1++;
 		s2++;
+		i++;
 	}
 
 
