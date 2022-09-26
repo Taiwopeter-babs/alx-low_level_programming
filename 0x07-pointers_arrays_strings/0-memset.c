64e5669a6ea86f5147cc43fd31157fcffc3c1929
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-26 09:56:28 +0000

    fill a memory block with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: pointer to memory to fill
+ * @b: constant byte to fill memory with
+ * @n: number of bytes to fill
+ * Return: to resulting pointer string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < n)
+	{
+		*s = b;
+		i++;
+		s++;
+	}
+
+
+	return (s);
+}
+
