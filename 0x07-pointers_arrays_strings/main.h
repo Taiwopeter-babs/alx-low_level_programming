a094ca28f110eafd6dc008a3ef96be2c7a73df46
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-26 11:07:05 +0000

    update header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 int string_length(char *string);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
