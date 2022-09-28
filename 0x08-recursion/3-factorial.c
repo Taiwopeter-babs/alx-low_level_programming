36de42fc26f357771344e257de6b7201522ce014
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-28 13:09:10 +0000

    compute factorial of a number with recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * factorial - computes factorial of a string recursively
+ * @n: parameter number
+ * Description: The function stores the multiplication of each factorial
+ * number of stacks and computes them when base value is fulfilled
+ * it follows LIFO stack principle.
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	/* base cases */
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+
+	/* recursive case terminates when base value is satisfied */
+	return (n * factorial(n - 1));
+
+	return (0);
+
+}
+
---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls the function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int f;
+
+	f = factorial(-1);
+	printf("%d\n", f);
+	return (0);
+}
