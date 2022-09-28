3baffe910e43582c8850d234e8bc83ccf73c4316
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-28 10:34:27 +0000

    print a string recursively

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string in recursive function
+ * @s: parameter string
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	/* base case */
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	
+	/* recursive case */
+	_puts_recursion(s);
+
+
+}
+
