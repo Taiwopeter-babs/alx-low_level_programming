4019bc556ec30d50e7a9d3bf812175ecb4ad5307
Taiwo Babalola <babalolataiwop@gmail.com>
2022-09-30 11:32:27 +0000

    multiply integer arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies the integer arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int product = 1;
+
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			product *= atoi(argv[i]);
+			i++;
+		}
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
