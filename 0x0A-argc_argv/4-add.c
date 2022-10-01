bfa0ec5fde815641a60f5be3e5a05ca29a3f8938
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-01 04:30:24 +0000

    use pointer notation to access and add integer elements on command line

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,19 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
  * main - adds the integer arguments
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Description: If only program name is passed, print 0;
  * if arguments > 1, check substrings of every
  * string, if it is a digit from 0-9, skip and check the next,
  * otherwise, print Error to stdout and return 1 i.e unsuccessful
  * Then, add the converted string to give the sum
- * Return: 0
+ * Return: 0 or 1 depending on input
  */
 int main(int argc, char **argv)
 {
@@ -32,7 +30,9 @@ int main(int argc, char **argv)
 			for (ptr_val = *param; *ptr_val != '\0'; ptr_val++)
 			{
 				if (*ptr_val >= '0' && *ptr_val <= '9')
+				{
 					continue;
+				}
 				else
 				{
 					printf("Error\n");
