f6978c474dbc2ad24de3aff74bd3273b3f0e215d
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-03 13:43:44 +0100

    update function to consider length if not NULL

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,31 +6,38 @@ int _strlen(char *s);
 /**
  * _strdup - copies a given string to new string with memmory allocation
  * @str: string to be duplicated
- *
+ * Description: if the original string is not null, calculate its length
  * Return: char or NULL if there's a failure in malloc or if str == NULL
  */
 char *_strdup(char *str)
 {
 	int i;
 	char *new_str;
-	int len = _strlen(str);
-
-	new_str = malloc((len + 1) * sizeof(char));
+	int len;
 
 	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (*(str + i) != '\0')
 	{
-		if (new_str != NULL)
-		{
-			*(new_str + i) = *(str + i);
-			i++;
-		}
-		else
-			return (NULL);
+		return (NULL);
 	}
-	*(new_str + i) = '\0';
+	if (str != NULL)
+	{
+		len = _strlen(str);
+
+		new_str = malloc((len + 1) * sizeof(char));
+
+		i = 0;
+		while (*(str + i) != '\0')
+		{
+			if (new_str != NULL)
+			{
+				*(new_str + i) = *(str + i);
+				i++;
+			}
+			else
+				return (NULL);
+		}
+		*(new_str + i) = '\0';
+	}
 
 	return (new_str);
 }
