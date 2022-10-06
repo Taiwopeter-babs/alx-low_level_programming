481f275edb987df991f401975b25508e11fae75f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-06 11:03:03 +0100

    allocate memory with respect to b

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory 
+ * @b: amount of memory to allocate
+ *
+ * Return: pointer or exit status 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+
+	return (ptr);
+}
