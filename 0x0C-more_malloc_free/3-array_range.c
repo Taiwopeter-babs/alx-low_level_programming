3c48276459f8f1cf33fb03fa4889eae435a96132
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-06 21:14:43 +0100

    create an array of integers

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * ((max - min) + 2));
+	ptr = malloc(sizeof(int) * (max + 2) - min);
 
 	if (ptr == NULL)
 		return (NULL);
