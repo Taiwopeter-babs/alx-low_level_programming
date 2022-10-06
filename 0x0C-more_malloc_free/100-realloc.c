81fd769dcb1002859e7fe0db104d3a3bb723bdec
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-06 22:46:31 +0100

    create custom realloc function

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates block of memory on the heap
+ * and frees the previously allocated memory
+ * @ptr: pointer to address of previously allocated memory heap
+ * @old_size: previous size of memory
+ * @new_size: new size of memory
+ * Return: pointer to newly allocated heap of memory, otherwise check
+ * conditions
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (ptr == NULL)
+		malloc(new_size);
+
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size > old_size)
+	{
+		free(ptr);
+		malloc(new_size);
+	}
+
+	return (ptr);
+}
