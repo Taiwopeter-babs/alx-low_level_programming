7682cda90bfd68d6b12c9f11d789c215ee6f0cab
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-06 22:55:52 +0100

    create custom realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (ptr == NULL)
-		malloc(new_size);
+		ptr = malloc(new_size);
 
 	if (ptr != NULL && new_size == 0)
 	{
