5054b7a79f8f98e2dfbb1f529ee41a891980c4b9
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-10 15:52:32 +0100

    print members of struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,18 +13,17 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 
-		if (d->name == NULL)
-			printf("Name: (nil)");
-		else
+		if (d->name)
 			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
 
 		printf("Age: %.6f\n", d->age);
 
-		if (d->owner == NULL)
-			printf("Owner: (nil)\n");
-		else
+		if (d->owner)
 			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
-	exit (98);
 
 }
