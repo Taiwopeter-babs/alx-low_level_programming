1d6e708541ee532686df9736306778c99fa38d53
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-10 17:20:06 +0100

    create an instance of struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,45 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	unsigned int i, name_len, owner_len;
 	dog_t *ptr_dog;
 
 	ptr_dog = malloc(sizeof(dog_t));
-	if (!(ptr_dog))
+	if (ptr_dog == NULL)
 		return (NULL);
+	/**
+	 * get the length of the string, then allocate memory to store string
+	 * Parse each char to the alloc memory space to store in memory.
+	 */
+	for (name_len = 0; name[name_len] != '\0'; name_len++)
+		;
 
-	ptr_dog->name = name;
+	ptr_dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (ptr_dog->name == NULL)
+	{
+		free(ptr_dog->name);
+		free(ptr_dog);
+		return (NULL);
+	}
+	for (i = 0; i < name_len; i++)
+		ptr_dog->name[i] = name[i];
+
+	/* initialize the age of the dog */
 	ptr_dog->age = age;
-	ptr_dog->owner = owner;
+
+	/* See ptr_dog->name for explanation */
+	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+		;
+
+	ptr_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (ptr_dog->owner == NULL)
+	{
+		free(ptr_dog->owner);
+		free(ptr_dog);
+		return (NULL);
+	}
+	for (i = 0; i < owner_len; i++)
+		ptr_dog->owner[i] = owner[i];
 
 	return (ptr_dog);
-
 }
