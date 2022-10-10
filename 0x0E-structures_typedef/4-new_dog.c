4175ab409f9d9c2f19ec2e8c2c28606435b7bd17
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-10 16:13:38 +0100

    create an instance of struct dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function creates an instance of dog_t
+ * a synonym of struct dog
+ * @name: member param
+ * @age: member param
+ * @owner: member param
+ *
+ * Return: pointer to type dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr_dog;
+
+	ptr_dog = malloc(sizeof(dog_t));
+	if (ptr_dog == NULL)
+		return (NULL);
+
+	ptr_dog->name = name;
+	ptr_dog->age = age;
+	ptr_dog->owner = owner;
+
+	return (ptr_dog);
+
+}
