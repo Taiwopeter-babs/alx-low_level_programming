b7e0c16791cc54f32e70b8f436cae8d386945c0c
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-10 13:19:41 +0100

    update header file for struct declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog 
+/**
+ * struct dog - structure declaration for dog
+ * @name: pointer to string name
+ * @age: age member
+ * @owner: pointer to string owner
+ *
+ * Description: This structure consists of three members
+ * which give the name, age and owner members with data types.
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 
 #endif
