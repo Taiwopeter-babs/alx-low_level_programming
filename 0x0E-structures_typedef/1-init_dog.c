f0ed27d6eae80c72b903fbe3f2d2e95fa610c2db
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-10 14:11:09 +0100

    intialize structure members with pointer of same struct type

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,10 +3,12 @@
 /**
  * init_dog - initializes the members of the structure dog
  * and parses a pointer variable to a structure
+ *
  * @d: pointer to structure
  * @name: member to be initialized
  * @age: member to be initialized
  * @owner: member to be initialized
+ *
  * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
