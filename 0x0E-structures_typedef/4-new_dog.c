7919a3e7ae60e542714f662c36e1989d1461142f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-11 11:42:09 +0100

    test code for edge cases

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,11 @@
 #include "dog.h"
 #include <stdlib.h>
-
 /**
  * new_dog - function creates an instance of dog_t
  * a synonym of struct dog
  * @name: member param
  * @age: member param
  * @owner: member param
- *
  * Return: pointer to type dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
@@ -16,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr_dog;
 
 	ptr_dog = malloc(sizeof(dog_t));
-	if (ptr_dog == NULL || !(name) || !(owner))
+	if (ptr_dog == NULL)
 	{
 		free(ptr_dog);
 		return (NULL);
@@ -25,38 +23,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 	 * get the length of the string, then allocate memory to store string
 	 * Parse each char to the alloc memory space to store in memory + '\0'
 	 */
-	for (name_len = 0; name[name_len] != '\0'; name_len++)
+	for (name_len = 0; name[name_len]; name_len++)
 		;
+	name_len++;
 
-	ptr_dog->name = malloc(sizeof(char) * (name_len + 1));
-	if (!(ptr_dog->name))
+	ptr_dog->name = malloc(sizeof(char) * name_len);
+	if (ptr_dog->name == NULL)
 	{
-		free(ptr_dog);
 		free(ptr_dog->name);
+		free(ptr_dog);
 		return (NULL);
 	}
+
 	for (i = 0; i < name_len; i++)
 		ptr_dog->name[i] = name[i];
-	ptr_dog->name[i] = '\0';
 
 	/* initialize the age of the dog */
-	if (ptr_dog->age)
-		ptr_dog->age = age;
-
+	ptr_dog->age = age;
 	/* See ptr_dog->name for explanation */
-	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+	for (owner_len = 0; owner[owner_len]; owner_len++)
 		;
+	owner_len++;
 
-	ptr_dog->owner = malloc(sizeof(char) * (owner_len + 1));
-	if (!(ptr_dog->owner))
+	ptr_dog->owner = malloc(sizeof(char) * owner_len);
+	if (ptr_dog->owner == NULL)
 	{
+		free(ptr_dog->name);
 		free(ptr_dog->owner);
 		free(ptr_dog);
 		return (NULL);
 	}
+
 	for (i = 0; i < owner_len; i++)
 		ptr_dog->owner[i] = owner[i];
-	 ptr_dog->owner[i] = '\0';
 
 	return (ptr_dog);
 }
