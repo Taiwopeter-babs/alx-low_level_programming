f656122bcd97ca18da8e048653b905c1a0f50874
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-12 22:01:42 +0100

    add operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - computes addition of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - computes subtraction of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - computes multilplication of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - computes division of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - computes modulo of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: value
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
