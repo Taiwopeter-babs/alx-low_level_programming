a6a54d288e4f2c3df034030795e738f9166fda8b
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-12 12:31:49 +0100

    print string with function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -  prints the name string
+ * @name: pointer to name string
+ * @f: pointer to string as argument
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *str))
+{
+	f(name);
+}
