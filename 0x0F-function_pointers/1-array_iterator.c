1409add125d1f7868efb759e72f28dfb596a0c57
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-12 12:52:16 +0100

    perform action on array with function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through an array and performs an "action"
+ * @array: array to be iterated
+ * @size: size of the array
+ * @action: pointer to function to perform the task
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
