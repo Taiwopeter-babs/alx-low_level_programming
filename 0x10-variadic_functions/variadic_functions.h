aaee94e8d309df1d6f4a85195c3da9ae731a184f
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-14 15:06:47 +0100

    update header file for variadic functions prototype

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct print - structure to take members
+ * @type: The type/format specifier
+ * @func: pointer to function that prints a format specified
+ */
+struct print 
+{
+	char *type;
+	void (*func)(va_list);
+};
+
+typedef struct print print_type;
+
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
