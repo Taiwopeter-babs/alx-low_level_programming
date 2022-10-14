a4ffa9708941480a555dfadfa642086a0e47f0b3
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-14 16:28:24 +0100

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,87 @@
 #include "variadic_functions.h"
 /**
+ * print_ch - prints a character
+ * @ch_args: variadic type va_list character
+ * Return: nothing
+ */
+void print_ch(va_list ch_args)
+{
+	printf("%c", va_arg(ch_args, int));
+}
+/**
+ * print_int - prints an integer
+ * @int_args: variadic type va_list integer
+ * Return: nothing
+ */
+void print_int(va_list int_args)
+{
+	printf("%d", va_arg(int_args, int));
+}
+/**
+ * print_flt - prints a floating number
+ * @flt_args: variadic type va_list floating
+ * Return: nothing
+ */
+void print_flt(va_list flt_args)
+{
+	printf("%f", va_arg(flt_args, double));
+}
+/**
+ * print_str - prints a string
+ * @str_args: variadic type va_list string arguments
+ * Return: nothing
+ */
+void print_str(va_list str_args)
+{
+	char *string = va_arg(str_args, char*);
+
+	if (string == NULL)
+		printf("(nil)");
+	printf("%s", string);
+}
+
+/**
  * print_all - print anything with separator
  * @format: string of format specifiers
  * Return: nothing
  */
 void print_all(const char *const format, ...)
 {
-	unsigned int i;
-	char ch;
-	char *str, *separator = "";
-	float flt;
-	const char *travelformat;
-	va_list _args;
+	unsigned int i, j;
+	print_type pr[] = {
+			{"c", print_ch},
+			{"s", print_str},
+			{"f", print_flt},
+			{"i", print_int},
+			{NULL, NULL}
+		};
+	char *separator =  "";
+	va_list v_args;
 
-	va_start(_args, format);
+	va_start(v_args, format);
 
-	travelformat = format;
-	while (*travelformat != '\0')
+	if (format)
 	{
-		printf("%s", separator);
-		switch (*travelformat)
+		i = 0;
+		while (format && format[i])
 		{
-			case 'c':
-				ch = va_arg(_args, int);
-				printf("%c", ch);
-				break;
-			case 'i':
-				i = va_arg(_args, int);
-				printf("%d", i);
-				break;
-			case 'f':
-				flt = va_arg(_args, int);
-				printf("%f", flt);
-				break;
-			case 's':
-				str = va_arg(_args, char*);
-				if (str != NULL)
-					printf("%s", str);
-				if (str == NULL)
-					printf("(nil)");
-				break;
-			default:
-				separator = ", ";
-				break;
-
+			j = 0;
+			while (pr[j].type != NULL)
+			{
+				if (*pr[j].type == format[i])
+				{
+					printf("%s", separator);
+					pr[j].func(v_args);
+					separator = ", ";
+					break;
+				}
+				j++;
+			}
+			i++;
 		}
-		++travelformat;
 	}
 	printf("\n");
-	va_end(_args);
+	va_end(v_args);
+
+
 }
