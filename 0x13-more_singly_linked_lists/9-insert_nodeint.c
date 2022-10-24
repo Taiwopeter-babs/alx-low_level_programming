fcac008fb5ed8c16afcdd5ea585dbd9c51332a1a
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-24 21:47:34 +0100

    add node at a specific index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - adds node at a specific index
  * @head: starting node of list
@@ -19,25 +18,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
-
 	if (idx == 0)
 	{
 		node->next = *head;
 		*head = node;
 		return (node);
 	}
-
 	temp = *head;
-
-	for (node_idx = 0; node_idx < idx - 1; node_idx++)
+	if (idx != 0)
 	{
-		temp = temp->next;
+		for (node_idx = 0; node_idx < idx - 1; node_idx++)
+		{
+			temp = temp->next;
+			if (temp->next == NULL)
+				return (NULL);
+		}
 	}
-
-	if (temp->next == NULL)
-		return (NULL);
 	node->next = temp->next;
 	temp->next = node;
-
 	return (node);
 }
