3d2d94f4207f8975b205e28fc38c23aec3ff0d42
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-27 13:32:27 +0100

    update header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* libraries */
+#include <stdlib.h>
+
+
+/* prototypes */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
+
