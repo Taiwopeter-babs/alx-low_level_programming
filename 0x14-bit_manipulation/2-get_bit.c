6602163594d114d4d1441f3e6de8e32fcd871c90
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-27 15:13:32 +0100

    get the value of bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
  * get_bit - get the value of a bit at an index
  * @n: parameter number
  * @index: index to be checked
- * Return: nothing
+ * Return: integer bit value
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
