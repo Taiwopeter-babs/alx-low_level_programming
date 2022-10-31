9ede934e8ae52961128bf6dba07c7ef597a05921
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-31 20:01:23 +0100

    create file and write content into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * create_file - reads a textfile and prints the content to stdout
+ * create_file - creates a text file and adds content
  * @filename: name of the file to be created
- * @text_content: content to read into the file
+ * @text_content: content to write into the file
  * Return: 1 on success, otherwise -1
  */
 int create_file(const char *filename, char *text_content)
