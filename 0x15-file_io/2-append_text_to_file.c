e24990d440e4ca647b51b5657f932aedea184035
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-31 20:30:56 +0100

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,17 +12,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	for (len = 0; text_content[len]; len++)
-		;
-
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd < 0)
 		return (-1);
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
 
-	sz_wr = write(fd, text_content, len);
-	if (sz_wr < 0)
-		return (-1);
+		sz_wr = write(fd, text_content, len);
+		if (sz_wr < 0)
+			return (-1);
 
-	close(fd);
-	return (1);
+		close(fd);
+		return (1);
+	}
+	return (-1);
 }
