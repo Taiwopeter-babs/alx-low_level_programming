d4e7ea8896406fbe3c43a401c80a14948d0ba69a
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-31 20:20:57 +0100

    append text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to file
+ * @filename: name of the file to be created
+ * @text_content: content to append into the file
+ * Return: 1 on success, otherwise -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	ssize_t fd, sz_wr, len;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	sz_wr = write(fd, text_content, len);
+	if (sz_wr < 0)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
