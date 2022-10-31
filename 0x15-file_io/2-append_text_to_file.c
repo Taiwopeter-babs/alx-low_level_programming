0a4b65cbecb8ac6cc2a57fe85ce8fc6bca05914e
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-31 20:27:01 +0100

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,8 +11,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
 
 	for (len = 0; text_content[len]; len++)
 		;
