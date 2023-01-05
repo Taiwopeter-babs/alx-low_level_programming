e8fb90efa5e029c07764355d6f46e3bc1f75bade
Taiwo Babalola <babalolataiwop@gmail.com>
2023-01-06 00:17:11 +0100

    [add] hashing function implementing djb2 algorithm

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -39,7 +39,8 @@ void free_linked_list(hash_node_t *list_head);
 void free_table(hash_table_t *table);
 hash_table_t *hash_table_create(unsigned long int size);
 
-/* 1-djb2.c hash function */
+/* 1-djb2.c */
 unsigned long int hash_djb2(const unsigned char *str);
-
+/* 2-key_index.c hashing function*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 # endif /* HASH_TABLES_H */
