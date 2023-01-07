caae27818b42b2fe33ebc9aeeb74d1046bf5d565
Taiwo Babalola <babalolataiwop@gmail.com>
2023-01-07 22:47:24 +0100

    create sorted linked list to order hash table

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,420 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - creates a new hash table
+ * @size: size of the table to be created
+ * Return: pointer to new table on success, else NULL
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *new_table;
+	unsigned long int idx;
+
+
+	new_table = malloc(sizeof(shash_table_t));
+	if (new_table == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	new_table->size = size;
+	new_table->shead = NULL;
+	new_table->stail = NULL;
+
+	new_table->array = calloc(new_table->size, sizeof(shash_node_t *));
+	if (!(new_table->array))
+	{
+		sfree_table(new_table);
+		return (NULL);
+	}
+	for (idx = 0; idx < new_table->size; idx++)
+	{
+		new_table->array[idx] = NULL; /* set each head to NULL */
+	}
+
+	return (new_table);
+}
+
+/**
+ * shash_table_set - adds an element to the hash table.
+ * If there is collision, adds it to indexed linked list
+ * @ht: pointer to hash table
+ * @key: used to get the index with the hash function
+ * @value: value to be added alongside the key to hash table
+ * Return: 1 on success, 0 on failure
+ */
+int shash_table_set(shash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	shash_node_t *new_node, *current_node;
+
+
+	/* get index */
+	index = key_index((unsigned char *) key, ht->size);
+	if (!ht || !key || *key == '\0')
+	{
+		sfree_table(ht);
+		return (0);
+	}
+
+	/* create node to add */
+	new_node = screate_node(key, value);
+	if (!new_node)
+	{
+		sfree_node(new_node);
+		return (0);
+	}
+
+	/* check if index is occupied */
+	current_node = ht->array[index];
+
+	if (current_node == NULL) /* index is empty, key non-existent */
+	{
+		/* add to table */
+		ht->array[index] = new_node;
+		ht->shead = add_to_sorted(ht, new_node);
+		return (1);
+	}
+	else
+		scollision_h(ht, index, new_node);
+
+	ht->shead = add_to_sorted(ht, new_node);
+	return (1);
+}
+
+/**
+ * add_to_sorted - inserts node in sorted linked list
+ * based on ASCII values
+ * @ht: pointer to hash table
+ * @node: pointer to node of sorted linked list
+ * Return: head of sorted linked list, otherwise NULL
+ */
+shash_node_t *add_to_sorted(shash_table_t *ht, shash_node_t *node)
+{
+	shash_node_t *curr, *next;
+	unsigned long int ascii_val, ascii_head, next_val, curr_val;
+
+	/* if sorted list is empty */
+	if (ht->shead == NULL)
+	{
+		ht->shead = node;
+		ht->stail = node;
+		return (ht->shead);
+	}
+
+	ascii_val = get_ascii(node->key);
+
+	if (!(ht->shead->snext))
+	{
+		ascii_head = get_ascii(ht->shead->key);
+		if (ascii_val >= ascii_head)
+		{
+			ht->shead->snext = node;
+			node->sprev = ht->shead;
+			ht->stail = node;
+			return (ht->shead);
+		}
+		else if (ascii_val <= ascii_head)
+		{
+			node->snext = ht->shead;
+			ht->shead->sprev = node;
+			ht->shead = node;
+			ht->stail = node->snext;
+			return (ht->shead);
+		}
+	}
+	else if (ht->shead->snext)
+	{
+		curr = ht->shead;
+
+		if (ascii_val <= get_ascii(ht->shead->key))
+		{
+			node->snext = ht->shead;
+			ht->shead->sprev = node;
+			ht->shead = node;
+			return (ht->shead);
+		}
+		while (curr)
+		{
+			curr_val = get_ascii(curr->key);
+
+			if (curr->snext)
+			{
+				next_val = get_ascii(curr->snext->key);
+
+				if (ascii_val >= curr_val && ascii_val <= next_val)
+				{
+					next = curr->snext;
+					node->snext = next;
+					curr->snext = node;
+					node->sprev = curr;
+					next->sprev = node;
+					return (ht->shead);
+				}
+			}
+			if (ascii_val >= curr_val && curr->snext == NULL)
+			{
+				node->sprev = curr;
+				curr->snext = node;
+				ht->stail = node;
+				return (ht->shead);
+			}
+			curr = curr->snext;
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * scollision_h - uses chaining method to handle collisions
+ * in the hash table
+ * @ht: pointer to hash table
+ * @index: index of array that holds the linked list; recipient of new node
+ * @node: new node to be added to linked list
+ * Return: nothing
+ */
+void scollision_h(shash_table_t *ht, unsigned long int index, shash_node_t *node)
+{
+	shash_node_t *head;
+
+	head = ht->array[index];
+
+	ht->array[index] = sadd_node_to_chain(head, node);
+}
+/**
+ * sadd_node_to_chain - adds node beginning of linked list
+ * @head: pointer to head of linked list
+ * @node: new node to be added
+ * Return: pointer to head of linked list
+ */
+shash_node_t *sadd_node_to_chain(shash_node_t *head, shash_node_t *node)
+{
+	shash_node_t *list_head, *temp;
+
+	if (!head)
+	{
+		list_head = screate_node(node->key, node->value);
+		head = list_head;
+		return (head);
+	}
+	else
+	{
+		temp = head;
+		while (temp)
+		{
+			if (strcmp(temp->key, node->key) == 0)
+				break;
+			temp = temp->next;
+		}
+		if (temp == NULL)
+		{
+			node->next = head;
+			head = node;
+		}
+		else
+		{
+			free(temp->value);
+			temp->value = strdup(node->value);
+			sfree_node(node);
+		}
+	}
+
+	return (head);
+}
+
+
+/**
+ * get_ascii - compute the ascii values of string/char and returns
+ * @key: key to compute
+ * Return: unsigned long int value
+ */
+unsigned long int get_ascii(char *key)
+{
+	unsigned long int idx, sum = 0;
+
+	for (idx = 0; key[idx]; idx++)
+		sum += key[idx];
+
+	return (sum);
+}
+
+/**
+ * shash_table_get - retrieves value of key from hash table
+ * @ht: pointer to hash table
+ * @key: key parameter
+ * Return: value of key, else NULL
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *node;
+
+	index = key_index((unsigned char *) key, ht->size);
+	if (index > ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	if (!node)
+		return (NULL);
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * screate_node - creates new node for array or linked list
+ * @key: key of node
+ * @value: value of node
+ * Return: new node created
+ */
+shash_node_t *screate_node(const char *key, const char *value)
+{
+	shash_node_t *new_node;
+
+	new_node = malloc(sizeof(shash_node_t));
+	if (new_node == NULL)
+	{
+		sfree_node(new_node);
+		return (NULL);
+	}
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		sfree_node(new_node);
+		return (NULL);
+	}
+	if (!value)
+		new_node->value = "";
+	else
+		new_node->value = strdup(value);
+
+	new_node->next = NULL;
+	new_node->snext = NULL;
+	new_node->sprev = NULL;
+
+	return (new_node);
+}
+/**
+ * shash_table_print - prints a hash table in order based on
+ * sorted linked list
+ * @ht: pointer to hash table
+ * Return: nothing
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *node;
+	char *separation = "";
+
+	if (ht == NULL)
+		return;
+
+	node = ht->shead;
+
+	putchar('{');
+	printf("%s", separation);
+
+	while (node)
+	{
+		printf("%s'%s': '%s'", separation, node->key,
+				node->value);
+		separation = ", ";
+
+		node = node->snext;
+	}
+
+	putchar('}');
+	printf("\n");
+}
+/**
+ * shash_table_print_rev - prints a hash table in order
+ * @ht: pointer to hash table
+ * Return: nothing
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *node;
+	char *separation = "";
+
+	if (ht == NULL)
+		return;
+
+	putchar('{');
+	printf("%s", separation);
+
+	node = ht->stail;
+
+	while (node)
+	{
+		printf("%s'%s': '%s'", separation, node->key,
+				node->value);
+		separation = ", ";
+
+		node = node->sprev;
+	}
+	putchar('}');
+	printf("\n");
+}
+
+/**
+ * sfree_linked_list - frees the linked list in each array
+ * @list_head: pointer to head of linked list
+ * Return: nothing
+ */
+void sfree_linked_list(shash_node_t *list_head)
+{
+	shash_node_t *temp;
+
+	while (list_head)
+	{
+		temp = list_head;
+		list_head = list_head->next;
+		sfree_node(temp);
+	}
+	free(list_head);
+}
+
+/**
+ * sfree_table - frees a hash table
+ * @table: pointer to table
+ * Return: nothing
+ */
+void sfree_table(shash_table_t *table)
+{
+	unsigned long int idx;
+
+	for (idx = 0; idx < table->size; idx++)
+	{
+		sfree_linked_list(table->array[idx]);
+	}
+
+	/*free_slinked_list(table->shead);*/
+
+	free(table->array);
+	free(table);
+}
+/**
+ * sfree_node - frees a node
+ * @node: node to be freed
+ * Return: nothing
+ */
+void sfree_node(shash_node_t *node)
+{
+	free(node->key);
+	free(node->value);
+	free(node);
+}
+/**
+ * shash_table_delete - frees/deletes a hash table
+ * @ht: pointer to hash table
+ * Return: nothing
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	sfree_table(ht);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -56,4 +56,62 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+
+/* Prototypes for sorted hash table */
+
+
+/**
+ * struct shash_node_s - Node of a sorted hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ * @sprev: A pointer to the previous element of the sorted linked list
+ * @snext: A pointer to the next element of the sorted linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - Sorted hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ * @shead: A pointer to the first element of the sorted linked list
+ * @stail: A pointer to the last element of the sorted linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+/* Functions' prototypes */
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+shash_node_t *add_to_sorted(shash_table_t *ht, shash_node_t *node);
+unsigned long int get_ascii(char *c);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+shash_node_t *screate_node(const char *key, const char *value);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void sfree_linked_list(shash_node_t *list_head);
+void sfree_table(shash_table_t *table);
+void sfree_node(shash_node_t *node);
+void shash_table_delete(shash_table_t *ht);
+void scollision_h(shash_table_t *ht, unsigned long int index, shash_node_t *node);
+shash_node_t *sadd_node_to_chain(shash_node_t *head, shash_node_t *node);
+
 # endif /* HASH_TABLES_H */
