eed763ca6b682fb52850a768e4412e6236b7fdb4
Taiwo Babalola <babalolataiwop@gmail.com>
2023-04-11 22:30:02 +0100

    add header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+# ifndef SEARCH_ALG0S_H
+# define SEARCH_ALGOS_H
+
+/* Libraries */
+#include <stdio.h>
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+# endif /* SEARCHALGOS_H */
