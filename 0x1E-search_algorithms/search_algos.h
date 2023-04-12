e5bee1c4e211f3dc7c2f3860775166ed238ff369
Taiwo Babalola <babalolataiwop@gmail.com>
2023-04-12 20:17:11 +0100

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,11 @@
 
 /* Libraries */
 #include <stdio.h>
+#include <math.h>
 
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 # endif /* SEARCHALGOS_H */
